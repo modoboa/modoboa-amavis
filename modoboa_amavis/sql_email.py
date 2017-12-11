b3c1b2d24a4bb54ac22620797df3c5080196aaf9
Andrew Fyfe <andrew@fyfe.gb.net>
2017-12-11 23:16:16 +0000

    make changes to SQLemail to work with modoboa/modoboa#1330
    also improve parsing of X-Amavis-Alert header

--- modoboa_amavis/sql_email.py
+++ modoboa_amavis/sql_email.py
@@ -1,14 +1,12 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
 """
 An email representation based on a database record.
 """
 
 from __future__ import unicode_literals
 
-import email
-
 from django.template.loader import render_to_string
-from django.utils.encoding import smart_str
 
 from modoboa.lib.email_utils import Email
 
@@ -21,34 +19,28 @@ class SQLemail(Email):
 
     def __init__(self, *args, **kwargs):
         super(SQLemail, self).__init__(*args, **kwargs)
-        fields = ["From", "To", "Cc", "Date", "Subject"]
-        for field in fields:
-            self.headers += [
-                {"name": field, "value": self.get_header(self.msg, field)}
-            ]
-            setattr(self, field, self.msg[field])
-        qreason = self.get_header(self.msg, "X-Amavis-Alert")
         self.qtype = ""
         self.qreason = ""
-        if qreason != "":
+
+        qreason = self.msg["X-Amavis-Alert"]
+        if qreason:
             if ',' in qreason:
-                self.qtype, self.qreason = qreason.split(',', 1)
+                self.qtype, qreason = qreason.split(',', 1)
             elif qreason.startswith("BAD HEADER SECTION "):
                 # Workaround for amavis <= 2.8.0 :p
                 self.qtype = "BAD HEADER SECTION"
-                self.qreason = qreason[19:]
+                qreason = qreason[19:]
 
-    @property
-    def msg(self):
-        """Get message's content."""
-        if self._msg is None:
-            mail_text = get_connector().get_mail_content(self.mailid)
-            self._msg = email.message_from_string(smart_str(mail_text))
-            self._parse(self._msg)
-        return self._msg
+            qreason = " ".join([x.strip() for x in qreason.splitlines()])
+            self.qreason = qreason
+
+    def _fetch_message(self):
+        return get_connector().get_mail_content(self.mailid)
 
     def render_headers(self, **kwargs):
-        return render_to_string("modoboa_amavis/mailheaders.html", {
-            "qtype": self.qtype, "qreason": self.qreason,
+        context = {
+            "qtype": self.qtype,
+            "qreason": self.qreason,
             "headers": self.headers,
-        })
+        }
+        return render_to_string("modoboa_amavis/mailheaders.html", context)
