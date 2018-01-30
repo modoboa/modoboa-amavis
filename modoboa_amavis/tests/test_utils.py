b1c690424b84904b4b32999e83a38423f00c4ee4
Andrew Fyfe <andrew@neptune-one.net>
2018-01-30 16:33:13 +0000

    add error detection code to fix_utf8_encoding
    If decoding fails try using chardet to detect the character set,
    otherwise fail gracefully and return the original string.
    
    modoboa/modoboa#1377

---
+++ modoboa_amavis/tests/test_utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase
+
+from modoboa_amavis.utils import fix_utf8_encoding
+
+
+class FixUTF8EncodingTests(SimpleTestCase):
+
+    """Tests for modoboa_amavis.utils.fix_utf8_encoding()."""
+
+    def test_4_byte_unicode(self):
+        value = "\xf0\x9f\x99\x88"
+        expected_output = "\U0001f648"  # == See No Evil Moneky
+        output = fix_utf8_encoding(value)
+        self.assertEqual(output, expected_output)
+
+    def test_truncated_4_byte_unicode(self):
+        value = "\xf0\x9f\x99"
+        expected_output = "\xf0\x9f\x99"
+        output = fix_utf8_encoding(value)
+        self.assertEqual(output, expected_output)
